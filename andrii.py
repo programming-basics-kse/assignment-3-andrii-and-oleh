2751d4682a9f742c6eaa4f1e405b3abef64baeaf
YevdokymovAndrii <ayevdokymov@kse.org.ua>
2024-11-27 12:34:09 +0200

    final version of task 1

--- andrii.py
+++ andrii.py
@@ -1,19 +1,8 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Andrii')
-
-parser.add_argument("file")
-
-subparsers = parser.add_subparsers(dest='command')
-
-
-medals_parser = subparsers.add_parser("medals")
-medals_parser.add_argument("country")
-medals_parser.add_argument("year")
-
-args = parser.parse_args()
-
-if args.command == "medals":
+def medals_command(args):
+    country = args.medals[0]
+    year = args.medals[1]
     medalists = []
     medals = {
         "Bronze": 0,
@@ -23,17 +12,18 @@ if args.command == "medals":
     country_in_list = False
     with open(args.file, "r") as datafile:
         next(datafile)
-        #minimum year is 1896
-        #all years are even
+        # minimum year is 1896
+        # all years are even
         for line in datafile:
             line = line.split("\t")
             line[-1] = line[-1][:-1]
-            if (args.country.capitalize() == line[6] or args.country == line[7]) and args.year == line[9] and line[-1] in medals:
-                print(", ".join(line))
-                medalists.append(", ".join(line))
+            if (country.capitalize() == line[6] or country.upper() == line[7]) and year == line[9] and line[
+                -1] in medals:
+                print(", ".join(line[1:]))
+                medalists.append(", ".join(line[1:]))
                 medals[line[-1]] += 1
                 country_in_list = True
-        if int(args.year) < 1896 and not int(args.year) % 2:
+        if int(year) < 1896 and not int(year) % 2:
             print("there weren't any olympiad this year")
             print("try entering even number greater than 1896")
         elif not country_in_list:
@@ -43,3 +33,21 @@ if args.command == "medals":
 
         print(f"Bronze: {medals["Bronze"]}, Silver: {medals['Silver']}, Gold: {medals['Gold']}")
 
+    if output_file != None:
+        with open(output_file, "w") as datafile:
+            datafile.write("\n".join(medalists))
+
+parser = argparse.ArgumentParser(description='Andrii')
+
+parser.add_argument("file")
+parser.add_argument( "-medals", "--medals", nargs=2)
+parser.add_argument("-output", "--output")
+
+args = parser.parse_args()
+
+output_file = args.output
+data_file = args.file
+
+
+if args.medals != None:
+    medals_command(args)
